a0b8c23f2097387519eab7eab820522a3312abdc
SDH0106 <ehdgusehdgns@naver.com>
2023-01-25 08:35:15 +0900

    Day60 글꼴교체

--- Assets/Scripts/5.Shop/DescriptionUI.cs
+++ Assets/Scripts/5.Shop/DescriptionUI.cs
@@ -54,23 +54,23 @@ public class DescriptionUI : MonoBehaviour
                 break;
 
             case 5:
-                descriptionText.text = "모든 공격력의 합산이 끝난후 곱해지는 공격력 배율값과 관련된 능력치. 각 캐릭터마다 다른 배율을 가진다.";
+                descriptionText.text = "모든 공격력의 합산이 끝난후 곱해지는 공격력 배율값과 관련된 능력치. 각 캐릭터마다 다른 배율을 가집니다.";
                 break;
 
             case 6:
-                descriptionText.text = "검, 총류 무기의 공격력와 관련된 능력치. 이 수치가 몬스터의 방어력 보다 낮으면 대미지가 들어가지 않는다.";
+                descriptionText.text = "검, 총류 무기의 공격력와 관련된 능력치. 이 수치가 몬스터의 방어력 보다 낮으면 대미지가 들어가지 않습니다.";
                 break;
 
             case 7:
-                descriptionText.text = "스태프류 무기의 공격력와 관련된 능력치. 이 수치가 몬스터의 방어력 보다 낮으면 대미지가 들어가지 않는다.";
+                descriptionText.text = "스태프류 무기의 공격력와 관련된 능력치. 이 수치가 몬스터의 방어력 보다 낮으면 대미지가 들어가지 않습니다.";
                 break;
 
             case 8:
-                descriptionText.text = "총, 스태프류 무기의 공격력와 관련된 능력치. 이 수치가 몬스터의 방어력 보다 낮으면 대미지가 들어가지 않는다.";
+                descriptionText.text = "총, 스태프류 무기의 공격력와 관련된 능력치. 이 수치가 몬스터의 방어력 보다 낮으면 대미지가 들어가지 않습니다.";
                 break;
 
             case 9:
-                descriptionText.text = "검류 무기의 공격력와 관련된 능력치. 이 수치가 몬스터의 방어력 보다 낮으면 대미지가 들어가지 않는다.";
+                descriptionText.text = "검류 무기의 공격력와 관련된 능력치. 이 수치가 몬스터의 방어력 보다 낮으면 대미지가 들어가지 않습니다.";
                 break;
 
             case 10:
